e9ff95bc3775c6361331c2511a2a1b691ea52c5d
nicolejara09 <84295051+nicolejara09@users.noreply.github.com>
2023-11-06 21:03:37 -0600

    inputs

--- src/components/FormBuilder.js
+++ src/components/FormBuilder.js
@@ -1,31 +1,41 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './FormBuilder.css';
-import { Button, Dropdown, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./FormBuilder.css";
+import { v4 as uuidv4 } from "uuid";
+import { Button, Dropdown } from "react-bootstrap";
 
-function TitleForm() {
-  const [titleValue, setTitleValue] = useState(''); // Estado para el valor del título
-  const [paragraphValue, setParagraphValue] = useState(''); // Estado para el valor del párrafo
+function TitleForm({ onTitleChange, onQuestionChange }) {
+  const [titleValue, setTitleValue] = useState("");
+  const [questionValue, setQuestionValue] = useState("");
 
+  const handleTitleChange = (e) => {
+    setTitleValue(e.target.value);
+    onTitleChange(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setQuestionValue(e.target.value);
+    onQuestionChange(e.target.value);
+  };
 
   return (
-    <div className='form'>
+    <div className="form">
       <h2>
-      <input
-        type="text"
-        style={{ /*border: 'none', */outline: 'none', background: 'transparent' }}
-        value={titleValue}
-        onChange={(e) => setTitleValue(e.target.value)}
-        placeholder="Title"
-      />
-</h2>
+        <input
+          type="text"
+          style={{ outline: "none", background: "transparent" }}
+          value={titleValue}
+          onChange={handleTitleChange}
+          placeholder="Form Title"
+        />
+      </h2>
       <p>
         <input
           type="text"
-          style={{ /*border: 'none', */ outline: 'none', background: 'transparent' }}
-          value={paragraphValue}
-          onChange={(e) => setParagraphValue(e.target.value)}
-          placeholder="Text here..."
+          style={{ outline: "none", background: "transparent" }}
+          value={questionValue}
+          onChange={handleDescriptionChange}
+          placeholder="Form Description Here"
         />
       </p>
     </div>
@@ -33,51 +43,242 @@ function TitleForm() {
 }
 
 function FormBuilder() {
-  const formTooltip = <Tooltip id="tooltip-pregunta">Add title</Tooltip>;
-  const preguntaTooltip = <Tooltip id="tooltip-pregunta">Add question</Tooltip>;
-  const respuestaTooltip = <Tooltip id="tooltip-respuesta">Add answer</Tooltip>;
+  const [questions, setQuestions] = useState([]);
+  const [titleValue, setTitleValue] = useState(""); 
+  const [questionValue, setQuestionValue] = useState(""); 
+  const [responseTypeSelected, setResponseTypeSelected] = useState(false);
 
-  const [titles, setTitles] = useState([]);
-
-  const addTitle = () => {
-    setTitles([...titles, <TitleForm key={titles.length} />]);
+  const addQuestion = () => {
+    const newQuestion = {
+      id: uuidv4(),
+      type: responseTypeSelected,
+      question: "",
+      answer: "",
+      options: responseTypeSelected === "multiple" ? [""] : [],
+    };
+    setQuestions([...questions, newQuestion]);
   };
 
+  const handleTitleChange = (title) => {
+    // Handle title change if needed
+  };
+
+  const handleDescriptionChange = (questionId, question) => {
+    const updatedQuestions = questions.map((q) =>
+      q.id === questionId ? { ...q, question: question } : q
+    );
+    setQuestions(updatedQuestions);
+  };
+
+  const handleQuestionChange = (questionId, newQuestion) => {
+    const updatedQuestions = questions.map((question) =>
+      question.id === questionId
+        ? { ...question, question: newQuestion }
+        : question
+    );
+    setQuestions(updatedQuestions);
+  };
+
+  const handleAnswerChange = (questionId, answer) => {
+    const updatedQuestions = questions.map((question) =>
+      question.id === questionId ? { ...question, answer: answer } : question
+    );
+    setQuestions(updatedQuestions);
+  };
+
+  const handleOptionChange = (questionId, optionIndex, option) => {
+    const updatedQuestions = questions.map((question) =>
+      question.id === questionId
+        ? {
+            ...question,
+            options: question.options.map((o, index) =>
+              index === optionIndex ? option : o
+            ),
+          }
+        : question
+    );
+    setQuestions(updatedQuestions);
+  };
+
+  const handleAddOption = (questionId) => {
+    const updatedQuestions = questions.map((question) =>
+      question.id === questionId
+        ? { ...question, options: [...question.options, ""] }
+        : question
+    );
+    setQuestions(updatedQuestions);
+  };
+
+  const handleRemoveOption = (questionId, optionIndex) => {
+    const updatedQuestions = questions.map((question) =>
+      question.id === questionId
+        ? {
+            ...question,
+            options: question.options.filter(
+              (_, index) => index !== optionIndex
+            ),
+          }
+        : question
+    );
+    setQuestions(updatedQuestions);
+  };
+  
+  const handleSelectResponseType = (type) => {
+    setResponseTypeSelected(type);
+  };
   return (
-    <div className="container">
-      <div className="fixed-right">
-        <h6 className="text-center mb-3">Controls</h6>
-        <div className="mb-1">
-          <OverlayTrigger placement="left" overlay={formTooltip}>
-            <Button variant="dark" onClick={addTitle}>
-              <i className="fa fa-bookmark"></i>
-            </Button>
-          </OverlayTrigger>
+    <div className="form-container">
+      <div className="container">
+        {/* Espacio para el título */}
+        <div className="row">
+          <div className="col-md-12">
+            <h2>Form Title</h2>
+            <input
+              type="text"
+              className="form-control"
+              value={titleValue}
+              onChange={handleTitleChange}
+              placeholder="Form Title"
+            />
+          </div>
+          <div className="col-md-12">
+            <p>Form Description Here</p>
+            <input
+              type="text"
+              className="form-control"
+              value={questionValue}
+              onChange={handleDescriptionChange}
+              placeholder="Form Description Here"
+            />
+          </div>
         </div>
 
-        <div className="mb-1">
-          <OverlayTrigger placement="left" overlay={preguntaTooltip}>
-            <Button variant="dark">
-              <i className="fa fa-question-circle"></i>
-            </Button>
-          </OverlayTrigger>
+        {/* Controles para agregar preguntas */}
+        <div className="row mt-3">
+          <div className="col-md-6">
+          <Button variant="dark" onClick={addQuestion} disabled={!responseTypeSelected}>
+            Add Question
+          </Button>
+          </div>
+          <div className="col-md-6">
+            <Dropdown>
+              <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                Select Response Type
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+              <Dropdown.Item onClick={() => handleSelectResponseType("text")}>
+        Text
+      </Dropdown.Item>
+      <Dropdown.Item onClick={() => handleSelectResponseType("number")}>
+        Number
+      </Dropdown.Item>
+      <Dropdown.Item onClick={() => handleSelectResponseType("multiple")}>
+        Multiple Choice
+      </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
         </div>
 
-        <OverlayTrigger placement="left" overlay={respuestaTooltip}>
-          <Dropdown>
-            <Dropdown.Toggle variant="dark" id="dropdown-basic">
-              <i className="fa fa-comment"></i>
-            </Dropdown.Toggle>
+        {/* Preguntas y respuestas */}
+        <div className="row mt-3">
+          {questions.map((question) => (
+            <div
+              key={question.id}
+              className="col-md-12 mb-3 question-container"
+            >
+              {question.type === "text" && (
+                <div>
+                  <h4>Text Question</h4>
+                  <input
+                    type="text"
+                    className="custom-input" // Agrega una clase personalizada
+                    placeholder="Question"
+                    value={question.question}
+                    onChange={(e) =>
+                      handleQuestionChange(question.id, e.target.value)
+                    }
+                  />
+                  <input
+                    type="text"
+                    className="custom-data-input"
+                    placeholder="Answer"
+                    value={question.answer}
+                    onChange={(e) =>
+                      handleAnswerChange(question.id, e.target.value)
+                    }
+                  />
+                </div>
+              )}
 
-            <Dropdown.Menu>
-              <Dropdown.Item href="#">Text</Dropdown.Item>
-              <Dropdown.Item href="#">Numbers</Dropdown.Item>
-              <Dropdown.Item href="#">Document</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </OverlayTrigger>
+              {question.type === "number" && (
+                <div>
+                  <h4>Number Question</h4>
+
+                  <input
+                    type="text"
+                    className="custom-input" // Agrega una clase personalizada
+                    placeholder="Question"
+                    value={question.question}
+                    onChange={(e) =>
+                      handleQuestionChange(question.id, e.target.value)
+                    }
+                  />
+                  <input
+                    type="number"
+                    className="custom-data-input"
+                    placeholder="Answer"
+                    value={question.answer}
+                    onChange={(e) =>
+                      handleAnswerChange(question.id, e.target.value)
+                    }
+                  />
+                </div>
+              )}
+
+              {question.type === "multiple" && (
+                <div>
+                  <h4>Multiple Choice Question</h4>
+                  <input
+                    type="text"
+                    className="custom-input" // Agrega una clase personalizada
+                    placeholder="Question"
+                    value={question.question}
+                    onChange={(e) =>
+                      handleQuestionChange(question.id, e.target.value)
+                    }
+                  />
+                  {question.options.map((option, index) => (
+                    <div key={index} className="mb-1 custom-option-container">
+                      <input
+                        type="text"
+                        className="custom-option-input"
+                        placeholder={`Option ${index + 1}`}
+                        value={option}
+                        onChange={(e) =>
+                          handleOptionChange(question.id, index, e.target.value)
+                        }
+                      />
+                      <Button
+                        variant="outline-danger"
+                        onClick={() => handleRemoveOption(question.id, index)}
+                      >
+                        Remove Option
+                      </Button>
+                    </div>
+                  ))}
+                  <Button
+                    variant="outline-dark"
+                    onClick={() => handleAddOption(question.id)}
+                  >
+                    Add Option
+                  </Button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="titles-container">{titles}</div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import 'font-awesome/css/font-awesome.min.css';
+import React, { useState } from 'react';
 
-
-import React from 'react';
 //  <img src="..\assets\iconForm.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top mx-3"/>
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark custom-navbar">
       <div className="container">
         <a className="navbar-brand custom-brand" href="#">
           <i className="fa fa-wpforms"></i> JN FORMS
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item mx-5 active">
-              <a className="nav-link custom-link" href="#">Preguntas</a>
+        <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item mx-3">
+              <a className="nav-link custom-link" href="#">
+                Preguntas
+              </a>
             </li>
-            <li className="nav-item mx-5 custom-link">
-              <a className="nav-link" href="#">Respuestas</a>
+            <li className="nav-item mx-3">
+              <a className="nav-link custom-link" href="#">
+                Respuestas
+              </a>
             </li>
           </ul>
         </div>
@@ -29,5 +38,4 @@ function Navbar() {
     </nav>
   );
 }
-
 export default Navbar;
